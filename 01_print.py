19a0398b7d7ba4889046f86dbc9b9e0bb2f7784f
mjaram73 <47092768+mjaram73@users.noreply.github.com>
2023-01-02 11:18:45 -0500

    Add files via upload
    Two First Files created during the course. Topics: Print and variables

---
+++ 01_print.py
@@ -0,0 +1,19 @@
+print("Hola, esto es el arhcivo 01")
+
+print("Hola, soy Nicolas y tengo 12 años")
+
+
+print(12 + 5)
+
+print(10-5)
+
+print(2*3)
+
+print(8/2)
+
+# Esto es un comentario
+
+"""
+Varias
+Lineas
+"""
---
+++ 02_vars.py
@@ -0,0 +1,21 @@
+print("Hola, vars")
+
+#Esto es una variable
+my_name = "Nicolas"
+print(my_name)
+
+#Esto es una variable con un numero
+my_age = 12
+print(my_age)
+
+#Las variables pueden cambiar de valor
+my_name = "Santiago"
+print("Aqui cambió",my_name)
+
+
+# input
+my_name = input("Cuál es tu nombre?")
+print("Usando input", my_name)
+
+my_age = input("Cuál es tu edad en años?")
+print("Usando input", my_age)
